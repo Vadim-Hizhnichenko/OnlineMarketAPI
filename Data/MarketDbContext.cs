acabcbaf90918df890104a4c722285f063221642
Vadim Hizhnichenko <yankeevadim@gmail.com>
2022-05-03 18:52:49 +0300

    Add project files.

---
+++ Data/MarketDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class MarketDbContext : DbContext
+    {
+        public MarketDbContext(DbContextOptions<MarketDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MarketAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Data;
+using Data.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly MarketDbContext _marketDbContext;
+
+        public ProductsController(MarketDbContext marketDbContext)
+        {
+            _marketDbContext = marketDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _marketDbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            return await _marketDbContext.Products.FindAsync(id);
+        }
+    }
+}
